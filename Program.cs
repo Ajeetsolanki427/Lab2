26544957afcaf9a5ce12d01456ff37209906d483
Ajeet Kumar Solanki <141894969+Ajeetsolanki427@users.noreply.github.com>
2023-08-23 09:00:59 +0530

    File added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Lab21
+{
+    class Employee
+    {
+        string name;
+        int age;
+        double salary;
+        public Employee(string name, int age, double salary)
+        {
+            this.name = name;
+            this.age = age; 
+            this.salary = salary;
+        }
+        public void display()
+        {
+            Console.WriteLine("Employee name : " + name);
+            Console.WriteLine("Employe age : " + age);
+            Console.WriteLine("Employe salary : " + salary);
+        }
+        public static void Main(string[] args)
+        {
+            Employee e1 = new Employee("Raghav", 23, 50000);
+            e1.display();
+        }
+    }
+}
